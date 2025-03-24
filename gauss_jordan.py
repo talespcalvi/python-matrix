5ce6f532292ac736e5a323c9357ecf803a9e57d2
Tales Paiva <124717378+talespcalvi@users.noreply.github.com>
2025-03-24 20:34:23 -0300

    Update gauss_jordan.py

--- gauss_jordan.py
+++ gauss_jordan.py
@@ -1,67 +1,61 @@
-# gauss_jordan.py  
+def solve_gauss_jordan(augMat, decimals=3):
+    def determinant(AM):
+        n = len(AM)
+        for fd in range(n):
+            if AM[fd][fd] == 0:
+                for j in range(fd + 1, n):
+                    if AM[j][fd] != 0:
+                        AM[fd], AM[j] = AM[j], AM[fd]
+                        break
+                else:
+                    raise ValueError("Matriz singular!")
 
-def print_matrix(M, decimal = 3):
-    for row in M:
-        print([round(x, decimal) + 0 for x in row])
+            for i in range(fd + 1, n):
+                crScaler = AM[i][fd] / AM[fd][fd]
+                for j in range(n):
+                    AM[i][j] -= crScaler * AM[fd][j]
 
-def zeros_matrix(rows, cols):
-    M = []
-    while len(M) < rows:
-        M.append([])
-        while len(M[-1]) < cols:
-            M[-1].append(0.0)
-    return M
+        product = 1.0
+        for i in range(n):
+            product *= AM[i][i]
+        return product
 
-matriz_zeros = zeros_matrix(3, 3)
-print_matrix(matriz_zeros)
+    def verifica_non_singularidade(A):
+        det = determinant([row[:] for row in A])
+        if det == 0:
+            raise ArithmeticError("Matriz Singular!")
 
-def GaussJordanMethod(augMat):
+    verifica_non_singularidade([row[:-1] for row in augMat])
+
     n = len(augMat)
     m = len(augMat[0])
-    #print(n)
-    #print(m)
 
     for i in range(n):
-    # Verificar se o pivo pode ser definido ou não
-        print(augMat)
         if augMat[i][i] == 0:
             for j in range(i + 1, n):
-                augMat[i], augMat[j] = augMat[j], augMat[i]
-                break
-        else:
-            # Caso não encontre linha com valor diferente de zero
-            # Temos um singularidade
-            raise ValueError("Matriz singular!")
-        print(augMat, '\n')
+                if augMat[j][i] != 0:
+                    augMat[i], augMat[j] = augMat[j], augMat[i]
+                    break
+            else:
+                raise ValueError("Matriz singular!")
 
-    # Normalizando cada linha
-    # L_i <-- L_i/a_ii
-        if augMat[i][i] != 1:
-            divisor = augMat[i][i]
-            for k in range(m):
-                augMat[i][k] /= divisor
-        else:
-            pass
+        divisor = augMat[i][i]
+        for k in range(m):
+            augMat[i][k] /= divisor
 
-        # Zerando as entradas do pivo
-        # L_i <-- L_j - a_ji * L_i
         for j in range(n):
             if i != j:
                 coef = augMat[j][i]
                 for k in range(m):
                     augMat[j][k] -= coef * augMat[i][k]
-            else:
-                pass
-        
-    print_matrix(augMat)
 
-    if i == 0:
-        print(divisor)
-        print(augMat[0])
+    solutions = [round(row[-1], decimals) for row in augMat]
+    print("Solução:", solutions)
+    return solutions
 
-
-#matrix = [[3.0, 2.0, -4.0, 3.0], [2.0, 3.0, 3.0, 15.0], [5.0, -3.0, 1.0, 14.0]]
-matrix = [[0, 2, 0, 1, 0], [2, 2, 3, 2, -2], [4, -3, 0, 1, -7], [4, -3, 0, 1, -7], [6, 1, -6, -5, 6]]
-# print(matrix)
-
-GaussJordanMethod(matrix)
+# Exemplo de uso:
+#matrix = [[3.0, 2.0, -4.0, 3.0], [2.0, 3.0, 3.0, 15.0], [5.0, -3, 1.0, 14.0]]
+matrix = [[2, 2, -3, 4],
+          [1, 3, 1, 11],
+          [2, 5, -4, 13]]
+solve_gauss_jordan(matrix)
